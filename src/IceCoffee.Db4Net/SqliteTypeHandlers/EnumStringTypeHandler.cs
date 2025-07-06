a5a66db5a2c1f1b939b27150acce113a090a85f8
IceCoffee <1249993110@qq.com>
2025-07-06 16:00:47 +0800

    Updated version and added enumeration type handler
    In the `IceCoffee.Db4Net.csproj` file, the version number is updated from `1.0.6` to `1.0.7`.
    
    In the `EnumStringTypeHandler.cs` file, a new Dapper type handler `EnumStringTypeHandler<T>` is added to convert enumeration types to strings for storage in the TEXT column of the database. This handler implements the `Parse` and `SetValue` methods, supports use in .NET Core and other frameworks, and provides flexible processing of enumeration types.

---
+++ src/IceCoffee.Db4Net/SqliteTypeHandlers/EnumStringTypeHandler.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System.Data;
+
+namespace IceCoffee.Db4Net.SqliteTypeHandlers
+{
+    /// <summary>
+    /// A generic Dapper type handler for converting Enums to and from strings 
+    /// for storage in a database TEXT column.
+    /// </summary>
+    /// <typeparam name="T">The Enum type to handle.</typeparam>
+    public class EnumStringTypeHandler<T> : SqlMapper.TypeHandler<T> where T : struct
+    {
+        /// <summary>
+        /// Parses a string value from the database back to its Enum representation.
+        /// </summary>
+        /// <param name="value">The object from the database (expected to be a string).</param>
+        /// <returns>The parsed Enum value.</returns>
+        public override T Parse(object value)
+        {
+            if (value == null || value is DBNull)
+            {
+                // Return the default value for the enum (usually the member with value 0)
+                // or throw an exception, depending on your business logic.
+                return default;
+            }
+
+            try
+            {
+                // Parse the string to the corresponding enum value, ignoring case.
+#if NETCOREAPP
+                return Enum.Parse<T>(value.ToString()!, true);
+#else
+                return (T)Enum.Parse(typeof(T), value.ToString()!, true);
+#endif
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidCastException($"Cannot convert '{value}' to enum type '{typeof(T).Name}'.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Sets the value of a database parameter from an Enum value.
+        /// </summary>
+        /// <param name="parameter">The database command parameter.</param>
+        /// <param name="value">The Enum value to be stored.</param>
+        public override void SetValue(IDbDataParameter parameter, T value)
+        {
+            parameter.DbType = DbType.String;
+            parameter.Value = value.ToString();
+        }
+    }
+}
